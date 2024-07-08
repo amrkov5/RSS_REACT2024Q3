56c51c2eedd81d5c88d055ea3efed743b78d2c88
Anton markov <amrkov5@gmail.com>
2024-07-08 20:07:27 +0300

    refactor: refactor Main.tsx class to func components

--- class-components/src/components/Main.tsx
+++ class-components/src/components/Main.tsx
@@ -1,38 +1,65 @@
-import { Component, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { APIResponse, MainProps } from '../types';
 import Card from './Card';
 import Loader from './Loader';
 import NotFoundBlock from './NotFoundBlock';
 
-class Main extends Component<MainProps, { dataToPaint: APIResponse | null }> {
-  isNothingFound(): boolean {
-    const { dataToPaint } = this.props;
-    return Boolean(dataToPaint?.count);
+function isNothingFound(dataToPaint: APIResponse | null): boolean {
+  return Boolean(dataToPaint?.count);
+}
+
+function Main({ dataToPaint, fetchError }: MainProps): ReactNode {
+  if (fetchError) {
+    throw new Error('Fetch Error');
   }
 
-  render(): ReactNode {
-    const { dataToPaint, fetchError } = this.props;
-    if (fetchError) {
-      throw new Error('Fetch Error');
-    }
-    return (
-      <main className="main">
-        <div className="cards-wrapper">
-          {!dataToPaint && <Loader />}
-          {!this.isNothingFound() && dataToPaint && <NotFoundBlock />}
-          {dataToPaint &&
-            this.isNothingFound() &&
-            dataToPaint.results.map((el) => (
-              <Card
-                resource={dataToPaint.resource}
-                data={el}
-                key={Date.parse(el.edited)}
-              />
-            ))}
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main className="main">
+      <div className="cards-wrapper">
+        {!dataToPaint && <Loader />}
+        {!isNothingFound(dataToPaint) && dataToPaint && <NotFoundBlock />}
+        {dataToPaint &&
+          isNothingFound(dataToPaint) &&
+          dataToPaint.results.map((el) => (
+            <Card
+              resource={dataToPaint.resource}
+              data={el}
+              key={Date.parse(el.edited)}
+            />
+          ))}
+      </div>
+    </main>
+  );
 }
+// class Main extends Component<MainProps> {
+//   // isNothingFound(): boolean {
+//   //   const { dataToPaint } = this.props;
+//   //   return Boolean(dataToPaint?.count);
+//   // }
+
+//   render(): ReactNode {
+//     const { dataToPaint, fetchError } = this.props;
+//     if (fetchError) {
+//       throw new Error('Fetch Error');
+//     }
+//     return (
+//       <main className="main">
+//         <div className="cards-wrapper">
+//           {!dataToPaint && <Loader />}
+//           {!this.isNothingFound() && dataToPaint && <NotFoundBlock />}
+//           {dataToPaint &&
+//             this.isNothingFound() &&
+//             dataToPaint.results.map((el) => (
+//               <Card
+//                 resource={dataToPaint.resource}
+//                 data={el}
+//                 key={Date.parse(el.edited)}
+//               />
+//             ))}
+//         </div>
+//       </main>
+//     );
+//   }
+// }
 
 export default Main;
