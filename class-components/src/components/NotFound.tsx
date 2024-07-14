57a7d86b8f65406b1758c711d145e6964f1e0213
Anton markov <amrkov5@gmail.com>
2024-07-14 15:22:59 +0300

    refactor: add some css for not found page

--- class-components/src/components/NotFound.tsx
+++ class-components/src/components/NotFound.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from 'react';
 
 function NotFound(): ReactNode {
   return (
-    <div>
-      <h1>Not found</h1>
+    <div className="not-found">
+      <h1>Oops. Not found...</h1>
     </div>
   );
 }
