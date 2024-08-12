def5221313b131155c5f0f5255dcb6cd97c4cae4
Anton markov <amrkov5@gmail.com>
2024-08-12 17:22:13 +0300

    config: install all necessary deps and make settings

---
+++ forms/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config({
+  name: 'main config',
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettierRecommended,
+  ],
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    prettier,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+});
